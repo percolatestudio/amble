34ef82b37d79e5fab08cde858c2000bbd9117c90
Tom Coleman <tom@thesnail.org>
2015-04-13 09:49:41 +1000

    Messing around — a bit of a WIP

--- client/main.js
+++ client/main.js
@@ -1,25 +1,47 @@
-Tracker.autorun(function() {
-  var latLng = Geolocation.latLng();
-  if (latLng && Meteor.userId()) {
-    Meteor.users.update(Meteor.userId(), {$set: {lastLocation: latLng}});
-    // tell watch
-  }
-});
+// Tracker.autorun(function() {
+//   var latLng = Geolocation.latLng();
+//   if (latLng && Meteor.userId()) {
+//     Meteor.users.update(Meteor.userId(), {$set: {lastLocation: latLng}});
+//     // tell watch
+//   }
+// });
+
+var _log = [];
+Meteor.setTimeout(function() {
+  console.log('Flushing log');
+  _.each(_log, function(line) {
+    console.log(line);
+  });
+  _log = [];
+}, 1000);
+var log = function() {
+  _log.push(_.map(arguments, EJSON.stringify).join(' '));
+};
 
 Meteor.startup(function() {
 
   document.addEventListener("deviceready", function() {
+    
     if (Meteor.isCordova) {
+      
+      
+      // temp while the autorun is disabled
+      window.navigator.geolocation.getCurrentPosition(function(location) {
+        console.log('Location from Phonegap');
+      });
+      
       var bgGeo = window.plugins.backgroundGeoLocation;
 
       var success = function(location) {
+        log('sending', location, 'to server');
         // 1. rest API call (which ends up updating user)
         var data = {
           location: location,
-          userToken: Meteor.user().services.resume.token
+          userToken: Accounts._storedLoginToken()
         };
       
         HTTP.post(Router.url('geolocation'), {data: data}, function() {
+          log('done');
           // 2. tell watch
         
           // 3. Tell bgGeo to close the bg thread
@@ -28,13 +50,15 @@ Meteor.startup(function() {
       };
 
       var fail = function() {
-        Log('bgGeo failure', arguments)
+        log('bgGeo failure', arguments)
       };
       var options = {};
 
+      log('starting up bgGeo');
       bgGeo.configure(success, fail, options);
 
       bgGeo.start();
     }
   });
 });
+
--- routes.js
+++ routes.js
@@ -3,6 +3,12 @@ Router.route('geolocation', {
   where: 'server',
   action: function() {
     console.log(this.request.body);
+    
+    this.response.writeHead(200, {
+      'Access-Control-Allow-Origin': 'http://meteor.local',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    });
+    
     this.response.end('done')
   }
 });
