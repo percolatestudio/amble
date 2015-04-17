70ebba25f04d40776d0dd808cfcf3ef2c104d97c
Tom Coleman <tom@thesnail.org>
2015-04-17 15:16:41 +1000

    Refactor routes a little

--- routes.js
+++ routes.js
@@ -1,38 +1,49 @@
+Iron.Router.hooks.allowCors = function() {
+  if (this.request.method === 'OPTIONS') {
+    this.response.writeHead(200, {
+      'Access-Control-Allow-Origin': 'http://meteor.local',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    });
+  
+    this.response.end();
+  } else {
+    this.next();
+  }
+}
+
+var userForToken = function(token) {
+  var hashedToken = Accounts._hashLoginToken(token);
+  return Meteor.users.findOne({'services.resume.loginTokens.hashedToken': hashedToken});
+}
+
+Iron.Router.hooks.checkUserToken = function() {
+  var data = this.request.body;
+  if (! data.userToken) {
+    this.response.statusCode = 401;
+    return this.response.end();
+  }
+  this.request.user = userForToken(data.userToken);
+  if (! this.request.user) {
+    this.response.statusCode = 401;
+    return this.response.end();
+  }
+  
+  this.next();
+}
+
+
 Router.route('geolocation', {
   path: '/api/geolocation',
   where: 'server',
+  onBeforeAction: ['allowCors', 'checkUserToken'],
   action: function() {
-    if (this.request.method === 'OPTIONS') {
-      this.response.writeHead(200, {
-        'Access-Control-Allow-Origin': 'http://meteor.local',
-        'Access-Control-Allow-Headers': 'Content-Type'
-      });
-    
-      this.response.end();
-    }
-    
     var data = this.request.body;
-    console.log('Got request');
-    console.log(data);
-    
-    // find the user with this login token
-    // TODO : better errors?
-    if (! data.userToken) {
-      this.response.statusCode = 401;
-      return this.response.end();
-    }
-    var hashedToken = Accounts._hashLoginToken(data.userToken);
-    var user = Meteor.users.findOne({'services.resume.loginTokens.hashedToken': hashedToken});
-    if (! user) {
-      this.response.statusCode = 401;
-      return this.response.end();
-    }
     
     var latLng = {
       lat: data.location.latitude,
       lng: data.location.longitude
     };
-    Meteor.users.updateLocation(user._id, latLng);
+    Meteor.users.updateLocation(this.request.user._id, latLng);
     
     // Send push notification if required.
     
