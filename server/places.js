c01df7ee74527cd080442d5ca73f20fc32a1e44a
Tom Coleman <tom@thesnail.org>
2015-04-24 13:24:36 +1000

    Added groupon API

--- server/places.js
+++ server/places.js
@@ -1,39 +1,9 @@
-Facebook = {
-  loadPlacesForUser: function(user) {
-    //console.log('Loading places for ', user.profile.name);
-    var url = 'https://graph.facebook.com/me?fields=likes.fields(id,name,location).limit(100)';
-    var token = user.services.facebook.accessToken;
-    var results = HTTP.get(url, {params: {access_token: token}});
-    var likes = results.data.likes.data;
-    var places = _.filter(likes, function(l) { 
-      return l.location && l.location.latitude;
-    });
-    
-    _.each(places, function(place) {
-      var doc = {
-        name: place.name,
-        userId: user._id,
-        location: {
-          type: "Point",
-          coordinates: [
-            place.location.longitude,
-            place.location.latitude
-          ]
-        },
-        metadata: place
-      };
-      
-      Places.upsert(_.pick(doc, 'name', 'userId'), doc);
-    });
-  }
-}
-
 Meteor.publish('places/list', function(latLng) {
   var self = this;
   
   var user = Meteor.users.findOne(self.userId);
   var interval = Meteor.setInterval(function() {
-    Facebook.loadPlacesForUser(user);
+    Groupon.loadPlacesForUser(user);
   }, 10000);
   
   self.onStop(function() {
@@ -45,7 +15,7 @@ Meteor.publish('places/list', function(latLng) {
 
 Meteor.methods({
   'places/load': function() {
-    Facebook.loadPlacesForUser(Meteor.user());
+    Groupon.loadPlacesForUser(Meteor.user());
   },
   'places/sendNearestToMe': function() {
     var nearest = Places.findNearest(Meteor.userId(), Meteor.user().profile.lastLocation);
