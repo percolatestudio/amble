cd258f77468c4eda9c011db2c559161d92fbd707
Tom Coleman <tom@thesnail.org>
2015-04-28 19:17:13 +1000

    Refactored and documented providers a little.

--- server/places.js
+++ server/places.js
@@ -1,9 +1,17 @@
 Places.loadPlacesForUser = function(user) {
   console.log('Loading places for ', user.profile.name);
 
-  Foursquare.loadPlacesForUser(user);
-  Facebook.loadPlacesForUser(user);
-  Yelp.loadPlacesForUser(user);
+  var latLng = user.lastLocation;
+  var places = [];
+  places.push(Foursquare.loadPlaces(latLng));
+  places.push(Yelp.loadPlaces(latLng));
+  // places.push(Groupon.loadPlaces(latLng));
+  
+  _.each(places, function(place) {
+    var doc = _.extend({userId: user._id}, place);
+    // TODO -- add a schema a check it here?
+    Places.upsert(_.pick(doc, 'name', 'userId'), doc);
+  });
 };
 
 Meteor.publish('places/list', function(latLng) {
--- server/providers/groupon.js
+++ server/providers/groupon.js
@@ -1,8 +1,29 @@
+// announcementTitle
+//   "Up to 56%  Off at Siam Orchid Traditional Thai Massage "
+// dealUrl
+// endAt
+// largeImageUrl
+// highlightsHtml
+//   "<p>Experienced massage therapists use traditional deep, yoga-like stretching and rhythmic pressure or aromatherapy techniques to banish tension</p>"
+// merchant.name
+// shortAnnouncementTitle
+//   "Traditional Thai Massage"
+// title
+//   "Thai Massage or Package with Optional Aromatherapy at Siam Orchid Traditional Thai Massage (Up to 56% Off)  "
+// options.0.title
+// options.0.discountPercent
+// options.0.value.amount
+// options.0.price.amount
+// options.0.discount.amount
+// options.0.redemptionLocations.name
+// options.0.redemptionLocations.streetAddress1/2
+// options.0.redemptionLocations.lat/lng
+
 var URL = 'https://partner-api.groupon.com/deals.json';
 var didWarn = false;
 
 Groupon = {
-  loadPlacesForUser: function(user) {
+  loadPlaces: function(latLng) {
     if (!Meteor.settings.groupon) {
       if (!didWarn) {
         didWarn = true;
@@ -12,44 +33,30 @@ Groupon = {
     }
     var params = {
       tsToken: 'US_AFF_0_' + Meteor.settings.groupon.affiliateId + '_212556_0',
-      // lat: user.profile.lastLocation.lat,
-      // lng: user.profile.lastLocation.lng,
-      lat: 37.7833,
-      lng: -122.4167, 
-      // LOL, in miles, good one groupon
-      // radius: Math.round(100 / 1.6),
-      // radius: 100,
+      lat: latLng.lat,
+      lng: latLng.lng,
+      radius: 100,
       limit: 10
     };
     
     
     console.log(params);
     var results = HTTP.get(URL, {params: params});
-    console.log(results);
+    var deals = results.data.deals;
     
-        //
-    //
-    // var likes = results.data.likes.data;
-    // var places = _.filter(likes, function(l) {
-    //   return l.location && l.location.latitude;
-    // });
-    //
-    // _.each(places, function(place) {
-    //   var doc = {
-    //     name: place.name,
-    //     userId: user._id,
-    //     location: {
-    //       type: "Point",
-    //       coordinates: [
-    //         place.location.longitude,
-    //         place.location.latitude
-    //       ]
-    //     },
-    //     metadata: place
-    //   };
-    //
-    //   Places.upsert(_.pick(doc, 'name', 'userId'), doc);
-    // });
+    return _.map(deals, function(deal) {
+      return {
+        name: deal.name,
+        // location: {
+        //   type: "Point",
+        //   coordinates: [
+        //     place.location.longitude,
+        //     place.location.latitude
+        //   ]
+        // },
+        metadata: deal
+      };
+    });
   }
 }
 
