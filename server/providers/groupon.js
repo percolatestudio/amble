1f09fadc1b3bae4e94b9b59254055fa29f9c1c41
Tom Coleman <tom@thesnail.org>
2015-05-05 12:22:54 +1000

    Wrapped groupon in try/catch

--- server/providers/groupon.js
+++ server/providers/groupon.js
@@ -31,69 +31,74 @@ var LOCAL_CATEGORIES = _.map([
 
 Groupon = {
   loadDeals: function(lastLocation) {
-    if (!Meteor.settings.groupon) {
-      if (!didWarn) {
-        didWarn = true;
-        Log.warn("FYI: Groupon is not configured.  Make sure to run the app with --settings");
+    try {
+      if (!Meteor.settings.groupon) {
+        if (!didWarn) {
+          didWarn = true;
+          Log.warn("FYI: Groupon is not configured.  Make sure to run the app with --settings");
+        }
+        return;
       }
-      return;
-    }
 
-    var params = {
-      lat: lastLocation.lat,
-      lng: lastLocation.lng,
-      filters: LOCAL_CATEGORIES.join(' OR '),
-      radius: 100,
-      limit: 10
-    };
-
-    var url;
-    // If we don't know, we assume the US
-    if (lastLocation.country && lastLocation.country !== 'US') {
-      url = INT_URL;
-      params.country_code = lastLocation.country;
-      params.tsToken = lastLocation.country + '_AFF_0_' +
-        Meteor.settings.groupon.intAffiliateId + '_212556_0';
-    } else {
-      url = US_URL;
-      params.tsToken = 'US_AFF_0_' +
-        Meteor.settings.groupon.usAffiliateId + '_212556_0';
-    }
-
-    var results = HTTP.get(url, {params: params});
-    var deals = results.data.deals;
-
-    return _.map(deals, function(deal) {
-      // TODO -- one deal per option or just pick the first?
-      var option = deal.options[0];
-      var location = option.redemptionLocations[0];
-
-      return {
-        merchant: deal.merchant.name,
-        description: deal.announcementTitle,
-        location: {
-          type: "Point",
-          coordinates: [
-            location.lng,
-            location.lat
-          ],
-          address: location.streetAddress1,
-          city: location.city,
-          postalCode: location.postalCode,
-          state: location.state,
-          country: location.country || lastLocation.country || 'US',
-        },
-
-        dealUrl: deal.dealUrl,
-        imageUrl: deal.largeImageUrl,
-
-        value: option.value.amount,
-        price: option.price.amount,
-        expiry: new Date(deal.endAt),
-
-        type: 'groupon',
-        metadata: deal
+      var params = {
+        lat: lastLocation.lat,
+        lng: lastLocation.lng,
+        filters: LOCAL_CATEGORIES.join(' OR '),
+        radius: 100,
+        limit: 10
       };
-    });
+
+      var url;
+      // If we don't know, we assume the US
+      if (lastLocation.country && lastLocation.country !== 'US') {
+        url = INT_URL;
+        params.country_code = lastLocation.country;
+        params.tsToken = lastLocation.country + '_AFF_0_' +
+          Meteor.settings.groupon.intAffiliateId + '_212556_0';
+      } else {
+        url = US_URL;
+        params.tsToken = 'US_AFF_0_' +
+          Meteor.settings.groupon.usAffiliateId + '_212556_0';
+      }
+
+      var results = HTTP.get(url, {params: params});
+      var deals = results.data.deals;
+
+      return _.map(deals, function(deal) {
+        // TODO -- one deal per option or just pick the first?
+        var option = deal.options[0];
+        var location = option.redemptionLocations[0];
+
+        return {
+          merchant: deal.merchant.name,
+          description: deal.announcementTitle,
+          location: {
+            type: "Point",
+            coordinates: [
+              location.lng,
+              location.lat
+            ],
+            address: location.streetAddress1,
+            city: location.city,
+            postalCode: location.postalCode,
+            state: location.state,
+            country: location.country || lastLocation.country || 'US',
+          },
+
+          dealUrl: deal.dealUrl,
+          imageUrl: deal.largeImageUrl,
+
+          value: option.value.amount,
+          price: option.price.amount,
+          expiry: new Date(deal.endAt),
+
+          type: 'groupon',
+          metadata: deal
+        };
+      });
+    }
+    catch (e) {
+      console.log(e.message);
+    }
   }
 }
